7048a3ade8948073cd192a0bad4d9048dc100da8
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-29 20:12:12 -0500

    Initialization and create the routes and controller and model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,32 @@
+class ArticlesController < ApplicationController
+
+	before_action :get_user
+
+	def index
+		@articles = Article.all?
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params[:article])
+		@article.save!
+	end
+
+	private
+
+	def article_params
+		params.require(article).permit(:body)
+	end
+
+	def get_user
+		@user = User.find(params[:user_id]) if params.key?(:user_id)
+	end
+	
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+	before_action :get_article
+
+	def index
+		if !@article
+			@comments = Comment.all
+		else
+			@comments = @article.comments
+		end
+	end
+
+	def new
+		@comment = comment.new
+	end
+
+	def create
+		@article.comments << Comment.create!(comments_params)
+		recirect_to article_path(@article.id)
+	end
+
+	private
+
+	def comments_params
+		params.require(:comment).permit(:body)
+	end
+
+	def get_article
+		@article = Article.find(params[:article_id]) if params.key?(article_id)
+	end
+
+end
